db6940db0d8725131f503f848ed9d08500321a75
Nick_Reed <somthndiff@yahoo.com>
2015-07-26 15:00:02 -0500

    final v1
    set functions such that cachesolve would work for all invertible square
    matrices

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions in tandem are desgined to create matrices
+## which can cache their inverses, and then recall that
+## cached data
 
-## Write a short comment describing this function
+
+## makeCacheMatrix creates a matrix which can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<- NULL
+  set <- function(y = matrix()){
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve is a function that calculates  the inverse of 
+## makeCacheMatrix, but first checks to see if a backup exists
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinv(m)
+    m
 }
